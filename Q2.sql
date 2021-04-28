2e8c5b0355737f8d3d18a839a71fa71575de2ac9
jspodek <73512558+jspodek@users.noreply.github.com>
2021-04-28 18:39:57 -0400

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,6 +1,8 @@
 --a)
 SELECT COUNT(OrderID)
 FROM Orders;
+-- There are a total of 196 orders
+
 --b)
 SELECT LastName
 FROM Employees
@@ -10,6 +12,8 @@ FROM Orders
 GROUP BY EmployeeID
 ORDER BY COUNT(EmployeeID) DESC
 LIMIT 1);
+-- The Employee with the LastName "Peacock" has the most orders
+
 --c)
 SELECT ProductName 
 FROM Products
@@ -21,9 +25,10 @@ SELECT OrderID
 FROM Orders
 WHERE Orders.CustomerID IN (
 SELECT CustomerID
-FROM [Customers]
+FROM Customers
 WHERE Country = 'Germany'))
 GROUP BY ProductID
 ORDER BY SUM(Quantity) DESC
 LIMIT 1);
+-- The best-selling item to German customers is "boston crab meat"
 
