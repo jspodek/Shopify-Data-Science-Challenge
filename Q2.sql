58b9245d4b5f71edf9f9e2252c9a42c250eedd84
jspodek <73512558+jspodek@users.noreply.github.com>
2021-04-28 18:37:25 -0400

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,1 +1,29 @@
+--a)
+SELECT COUNT(OrderID)
+FROM Orders;
+--b)
+SELECT LastName
+FROM Employees
+WHERE EmployeeID IN (
+SELECT EmployeeID
+FROM Orders
+GROUP BY EmployeeID
+ORDER BY COUNT(EmployeeID) DESC
+LIMIT 1);
+--c)
+SELECT ProductName 
+FROM Products
+WHERE ProductID IN(
+SELECT ProductID
+FROM OrderDetails
+WHERE OrderID IN (
+SELECT OrderID
+FROM Orders
+WHERE Orders.CustomerID IN (
+SELECT CustomerID
+FROM [Customers]
+WHERE Country = 'Germany'))
+GROUP BY ProductID
+ORDER BY SUM(Quantity) DESC
+LIMIT 1);
 
